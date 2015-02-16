ce5a5dc2e4bfffc05a3db236b7e8cea881ef1c88
mahish <nezapomenout@gmail.com>
2015-02-16 16:47:03 +0100

    Initial
    starter-kit-jekyll-compass 6.12.2014

---
+++ _sass/config.rb
@@ -0,0 +1,32 @@
+require 'compass/import-once/activate'
+require 'susy'
+require 'breakpoint'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "./"
+css_dir = "../assets/css"
+images_dir = "../assets/img"
+javascripts_dir = "../assets/js"
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+preferred_syntax = :scss
+output_style = :compressed #expanded
+relative_assets = true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
